bccb0dcd2535e9442a663ada08a63330fcf33b9b
Einzel <einzel0102@mail.ru>
2023-04-16 00:39:33 +0800

    Задание №5 (доп) Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке.

---
+++ src/main/java/TaskJava5.java
@@ -0,0 +1,10 @@
+/*
+Задание №5 (доп)
+Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке.
+*/
+public class TaskJava5 {
+    public static void main(String[] args) {
+
+    }
+
+}
