bb6695d370b396cde9420f7524f8a6bf29864eaf
Einzel <einzel0102@mail.ru>
2023-04-16 12:37:42 +0800

    Задание №5 (доп) Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке.

--- src/main/java/TaskJava5.java
+++ src/main/java/TaskJava5.java
@@ -2,9 +2,23 @@
 Задание №5 (доп)
 Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке.
 */
+import java.util.Arrays;
 public class TaskJava5 {
     public static void main(String[] args) {
-
+        String words = "Добро пожаловать на курс по Java";
+        String[] arrayWords = words.split(" ");
+        System.out.println(Arrays.toString(arrayWords));
+        String temp = "";
+        for ( int i = 0; i < arrayWords.length / 2; i++) {
+            temp = arrayWords[i];
+            arrayWords[i] = arrayWords[arrayWords.length - i -1];
+            arrayWords[arrayWords.length - i -1] = temp;
+        }
+        System.out.println(Arrays.toString(arrayWords));
+        for (String j: arrayWords) {
+            System.out.print(j + ' ');
+        }
     }
+
 
 }
